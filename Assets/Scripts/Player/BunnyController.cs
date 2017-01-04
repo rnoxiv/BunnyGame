757fafd050da23c834e6582f3bed5ad8bd113dd4
rnoxiv <sabatier.david.93@gmail.com>
2017-01-04 20:48:41 +0100

    added score

--- Assets/Scripts/Player/BunnyController.cs
+++ Assets/Scripts/Player/BunnyController.cs
@@ -1,31 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BunnyController : MonoBehaviour {
 
     private Rigidbody2D rbody;
     private Animator anim;
+    private Collider2D collid;
+    private float startTime;
+    private float deathTime = -1;
 
-    public float JumpForce = 300f;
-    public float Speed = 20f;
-    private int numJumped = 0;
+    public Text scoreText;
+    public float Score = 0;
+    public float JumpForce = 750f;
+
 	// Use this for initialization
 	void Start () {
         rbody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        collid = GetComponent<Collider2D>();
+
+        startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //rbody.AddForce(transform.right * Speed);
-        if (Input.GetButtonUp("Jump") && numJumped != 2){
-            rbody.AddForce(transform.up * JumpForce);
-            numJumped++;
+        if(deathTime == -1)
+        {
+            if (Input.GetButtonUp("Jump")){
+                rbody.AddForce(transform.up * JumpForce);
+            }
+            anim.SetFloat("JVelocity", rbody.velocity.y);
+            scoreText.text = (Time.time - startTime).ToString("0.0");
         }else
         {
-            numJumped = 0;
+
+            if(Time.time > (deathTime + 2))
+                Application.LoadLevel(Application.loadedLevel);
         }
-        anim.SetFloat("JVelocity", rbody.velocity.y);
+        
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            foreach(MoveLeft movelefter in FindObjectsOfType<MoveLeft>())
+            {
+                movelefter.enabled = false;
+            }
+            foreach (ElementSpawner spawner in FindObjectsOfType<ElementSpawner>())
+            {
+                spawner.enabled = false;
+            }
+            deathTime = Time.time;
+            anim.SetBool("Dead", true);
+            rbody.velocity = Vector2.zero;
+            collid.enabled = false;
+            rbody.AddForce(transform.up * JumpForce);
+            Score = 0;
+        }
+            
+    }
 }
